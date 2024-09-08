f2ecd2be48c99ce4786b62aec02a03e1f3e3f7a0
Jimmy Kueh <jimmykzm94@gmail.com>
2024-09-08 18:02:39 +0800

    add cmake examples

---
+++ 03_version_string/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "version.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "Version major: " << VERSION_MAJOR << endl;
+    cout << "Version minor: " << VERSION_MINOR << endl;
+}
